5d828d7a4035322ddf240cf981ae9f330ef665c8
Cloudy17g35 <ksawery.lejczak@gmail.com>
2022-08-11 19:58:15 +0200

    formating code, applying small changes

--- app/app.py
+++ app/app.py
@@ -7,16 +7,19 @@ from data_visualizations import plotly_bar_chart, make_wordcloud
 from typing import List
 from collections import Counter
 from wordcloud import WordCloud
-WORDCLOUD_FILE = 'wordcloud.png'
 PUBLIC_KEY = 's3://bob-dylan-songs/dylan_songs.parquet'
-
+WORDCLOUD_FILENAME = 'wordcloud.png'
 
 def header_and_description():
-    st.write(TextDescription.HEADER.value, 
-             unsafe_allow_html=True)
+    st.write(
+        TextDescription.HEADER.value, 
+        unsafe_allow_html=True
+    )
     st.image(TextDescription.PICTURE.value)
-    st.write(TextDescription.DESCRIPTION.value,
-             unsafe_allow_html=True)
+    st.write(
+        TextDescription.DESCRIPTION.value, 
+        unsafe_allow_html=True
+    )
 
 
 def show_dataset(data_frame:pd.DataFrame):
@@ -27,49 +30,90 @@ def show_dataset(data_frame:pd.DataFrame):
 
 def barchart(data_frame=pd.DataFrame):
     fig = plotly_bar_chart(data_frame)
-    st.write(TextDescription.PLOT_SUBHEADER.value,
-             unsafe_allow_html=True)
+    st.write(
+        TextDescription.PLOT_SUBHEADER.value,
+        unsafe_allow_html=True
+        )
     st.plotly_chart(fig)
 
 
-def album_selection(data_frame:pd.DataFrame):
-        st.write(TextDescription.ALBUM_SELECTION.value,
-                unsafe_allow_html=True)
-        options:List[str] = ['select'] + list(data_frame['album'].unique())
-        option = st.selectbox('Album name:', options)
-        if option != 'select':
-            st.write(f'you selected {option}')
-            selected_df = data_frame[data_frame['album'] == option]
-            st.dataframe(selected_df)
-            df_to_download:pd.DataFrame = selected_df.to_csv().encode('utf8')
-            file_name_for_download:str = f'songs_from_album_{"_".join(option.lower().split())}.csv'
-            st.download_button(TextDescription.DOWNLOAD_DATAFRAME.value, 
-                            data=df_to_download,
-                                file_name=file_name_for_download,
-                                mime='text/csv')
+def get_options(data_frame:pd.DataFrame) -> List[str]:
+    return ['select'] + list(data_frame['album'].unique())
 
-   
+
+def get_filename_for_selected_option(option:str):
+    return f'songs_from_album_{"_".join(option.lower().split())}.csv'
+
+
+def save_dataframe_to_csv(
+    data_frame:pd.DataFrame, 
+    encoding='utf8') -> str:
+    
+    return data_frame.to_csv(
+        encoding=encoding
+        )
+    
+
+def album_selection(data_frame: pd.DataFrame):
+    st.write(
+        TextDescription.ALBUM_SELECTION.value, 
+        unsafe_allow_html=True
+    )
+    options: List[str] = get_options(data_frame)
+    option = st.selectbox("Album name:", options)
+    if option != "select":
+        st.write(f"selected option is {option}")
+        selected_df = data_frame[data_frame["album"] == option]
+        st.dataframe(selected_df)
+        csv_data:str = save_dataframe_to_csv(selected_df)
+        file_name_for_download:str = get_filename_for_selected_option(option)
+        button_label:str = TextDescription.DOWNLOAD_DATAFRAME.value
+        st.download_button(
+            label=button_label,
+            data=csv_data,
+            file_name=file_name_for_download,
+            mime="text/csv",
+        )
+
+
+def save_wordcloud_to_file(
+    wordcloud:WordCloud,
+    filename:str
+    ):
+    
+    wordcloud.to_file(filename)
+    
+
+
 def wordcloud(data_frame=pd.DataFrame):
-        st.write(TextDescription.WORDCLOUD_SUBHEADER.value,
-                unsafe_allow_html=True)
-        st.write(TextDescription.WORDCOUD_DESCRIPTION.value,
-                unsafe_allow_html=True)
-        years = list(data_frame['release_year'].unique())
-        selected_year = st.selectbox('Year:', years)
-        df_for_one_year = data_frame[data_frame['release_year'] == selected_year]
-        tp = TextPreprocessing()
-        lyrics: list = tp.clean_lyrics(df_for_one_year)
-        lyrics_counter:Counter = tp.get_counter(lyrics)
-        # wordcloud is saved as image in the background
-        wc:WordCloud = make_wordcloud(lyrics_counter).to_file('wordcloud.png')
-        st.image(WORDCLOUD_FILE)
-        with open(WORDCLOUD_FILE, 'rb') as file:
-            st.download_button(
-                label=TextDescription.DOWLOAD_WORDCLOUD.value,
-                data=file,
-                file_name=WORDCLOUD_FILE,
-                mime='image/png'
-            )
+    
+    st.write(
+        TextDescription.WORDCLOUD_SUBHEADER.value, 
+        unsafe_allow_html=True
+    )
+    st.write(
+        TextDescription.WORDCOUD_DESCRIPTION.value, 
+        unsafe_allow_html=True
+        )
+    
+    years = list(data_frame["release_year"].unique())
+    selected_year = st.selectbox("Year:", years)
+    df_for_one_year = data_frame[data_frame["release_year"] == selected_year]
+    tp:TextPreprocessing = TextPreprocessing()
+    lyrics: list = tp.clean_lyrics(df_for_one_year)
+    lyrics_counter: Counter = tp.get_counter(lyrics)
+    # wordcloud is saved as image in the background
+    wordcloud:WordCloud = make_wordcloud(lyrics_counter)
+    save_wordcloud_to_file(wordcloud, WORDCLOUD_FILENAME)
+    st.image(WORDCLOUD_FILENAME)
+    with open(WORDCLOUD_FILENAME, "rb") as file:
+        st.download_button(
+            label=TextDescription.DOWLOAD_WORDCLOUD.value,
+            data=file,
+            file_name=WORDCLOUD_FILENAME,
+            mime="image/png",
+        )
+
 
 
 if __name__ == '__main__':
--- app/text_descriptions.py
+++ app/text_descriptions.py
@@ -1,60 +1,64 @@
-'''this module contains text 
-descrpitons for applications'''
+"""this module contains
+text descrpitons,
+pictures for the application"""
+
 from enum import Enum
 
 
 class TextDescription(Enum):
+
+    HEADER = "<h1 style='text-align: center;" \
+             "color: blue;'>BOB DYLAN SONGS APP</h1>"
+
+    IMAGE = "picture.png"
+
+    DESCRIPTION = "<b>DESCRIPTION</b>" \
+
+    "This app enables you to see Bob Dylan songs distribution by year,"  \
+    "download dataset for particular album and make cloud of words for particular year" \
+
+    "<b>Content</b>:" \
+
+    "There are 4 columns:" \
+
+    "* release_year - year when song was released first time, " \
+    "* album - name of the album where track occurs," \
+    "* title - title of the song," \
+    "* lyrics - lyrics of the track" \
+
+    "<b>Acknowledgements</b>:" \
+
+    "contains only songs that Bob Dylan himself has" \
+    "written and published. There's many songs that Bob Dylan " \
+    "only covered so I didn't include them. " \
+    "For instance album <i>World Gone Wrong</i> contains " \
+    "only old folks songs." \
+
+    "<b>Inspiration</b>:" \
+
+    "I'm great Bob Dylan fan. I listen to his songs almost every" \
+    "day from many years. " \
+    "I also play them and sing them so now " \
+    "I decided to make dataset and play with them on this app as well. " \
+
+    PICTURE = "picture.png"
+
+    DATASET = "<h2 style='text-align: center;" \
+              "color: blue;'>DATASET</h2>"
+
+    PLOT_SUBHEADER = "<h2 style='text-align: center;" \
+                     "color: blue;'>SONGS DISTRIBUTION BY YEARS</h2>"
+
+    ALBUM_SELECTION = "Select album: "
+
+    DOWNLOAD_DATAFRAME = "DOWNLOAD YOUR DATAFRAME"
+
+    WORDCLOUD_SUBHEADER = "<h2 style='text-align: center;" \
+                          "color: blue;'>WORDCLOUD</h2>"
+                          
+    WORDCOUD_DESCRIPTION = "If you want to generate wordcloud " \
+                           "for particular year, select " \
+                           "this year on the slider, " \
+                           "wordcloud will be generated automatically"
     
-    HEADER = """<h1 style='text-align: center;
-color: blue;'>BOB DYLAN SONGS APP</h1>"""
-
-    IMAGE = 'picture.png'
-
-    DESCRIPTION = """<b>DESCRIPTION</b>
-
-This app enables you to see Bob Dylan songs distribution by year, 
-download dataset for particular album and make cloud of words for particular year
-
-<b>Content</b>:
-
-There are 4 columns:
-
-* release_year - year when song was released first time,
-* album - name of the album where track occurs,
-* title - title of the song,
-* lyrics - lyrics of the track 
-
-<b>Acknowledgements</b>:
-
-contains only songs that Bob Dylan himself has 
-written and published. There's many songs that Bob Dylan 
-only covered so I didn't include them. 
-For instance album <i>World Gone Wrong</i> contains 
-only old folks songs.
-
-<b>Inspiration</b>:
-
-I'm great Bob Dylan fan. I listen to his songs almost every
-day from many years. 
-I also play them and sing them so now 
-I decided to make dataset and play with them on this app as well. """
-    
-    PICTURE = 'picture.png'
-    
-    DATASET = """<h2 style='text-align: center;
-color: blue;'>DATASET</h2>"""
-    
-    PLOT_SUBHEADER = """<h2 style='text-align: center;
-color: blue;'>SONGS DISTRIBUTION BY YEARS</h2>"""
-    
-    ALBUM_SELECTION = 'Select album: '
-    
-    DOWNLOAD_DATAFRAME = 'DOWNLOAD YOUR DATAFRAME'
-    
-    WORDCLOUD_SUBHEADER = """<h2 style='text-align: center;
-color: blue;'>WORDCLOUD</h2>"""
-    WORDCOUD_DESCRIPTION = """If you want to generate wordcloud for particular year, select 
-this year on the slider, wordcloud will be generated 
-automatically"""
-    DOWLOAD_WORDCLOUD = 'DOWNLOAD YOUR WORDCLOUD'
-
+    DOWLOAD_WORDCLOUD = "DOWNLOAD YOUR WORDCLOUD"
