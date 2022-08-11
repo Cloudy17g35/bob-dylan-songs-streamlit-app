019f0c672039dcdcc362b0d6e84b9c3a9c79f199
Cloudy17g35 <ksawery.lejczak@gmail.com>
2022-08-11 22:30:01 +0200

    chanign text descriptions

--- app/text_descriptions.py
+++ app/text_descriptions.py
@@ -12,34 +12,40 @@ class TextDescription(Enum):
 
     IMAGE = "picture.png"
 
-    DESCRIPTION = "<b>DESCRIPTION</b>" \
-
-    "This app enables you to see Bob Dylan songs distribution by year,"  \
-    "download dataset for particular album and make cloud of words for particular year" \
-
-    "<b>Content</b>:" \
-
-    "There are 4 columns:" \
-
-    "* release_year - year when song was released first time, " \
-    "* album - name of the album where track occurs," \
-    "* title - title of the song," \
-    "* lyrics - lyrics of the track" \
-
-    "<b>Acknowledgements</b>:" \
-
-    "contains only songs that Bob Dylan himself has" \
-    "written and published. There's many songs that Bob Dylan " \
-    "only covered so I didn't include them. " \
-    "For instance album <i>World Gone Wrong</i> contains " \
-    "only old folks songs." \
-
-    "<b>Inspiration</b>:" \
-
-    "I'm great Bob Dylan fan. I listen to his songs almost every" \
-    "day from many years. " \
-    "I also play them and sing them so now " \
-    "I decided to make dataset and play with them on this app as well. " \
+    DESCRIPTION = """
+    <b>DESCRIPTION</b> \
+    \n
+    This app enables you to see Bob Dylan songs distribution by year, \
+    download dataset for particular album \
+    and make cloud of words for particular year \
+    \n
+    <b>CONTENT</b>: \
+    \n
+    There are 4 columns: \
+    \n
+    * release_year - year when song was released first time, \
+    \n
+    * album - name of the album where track occurs,  \
+    \n
+    * title - title of the song,  \
+    \n
+    * lyrics - lyrics of the track  \
+    \n
+    <b>ACKNOWLEDGEMENTS</b>: \
+    \n
+    Contains only songs that Bob Dylan himself has \
+    written and published. There's many songs that Bob Dylan " \
+    only covered so I didn't include them.  \
+    For instance album <i>World Gone Wrong</i> contains \
+    only old folks songs. \
+    \n
+    <b>INSPIRATION</b>: \
+    \n
+    I'm great Bob Dylan fan. I listen to his songs almost every \
+    day from many years.  \
+    I also play them and sing them so now \
+    I decided to make dataset and play with them on this app as well. \
+    with them on this app as well. """
 
     PICTURE = "picture.png"
 
